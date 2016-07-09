38db1334d05eaa619153be1745a0abbe9a741e5a
Lunar <lunar.developer@gmail.com>
2016-07-10 01:04:07 +0700

    :package: Add Project UnitTest

---
+++ src/Test/UnitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LunarSoft.UnitTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ src/Test/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LunarSoft")]
+[assembly: AssemblyProduct("LunarSoft.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("77b33917-e484-4c3b-97bf-e0120ada4a36")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
